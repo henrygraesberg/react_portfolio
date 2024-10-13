b16144f3b0dcee5b63abede0efce038a73e8e15a
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-10-13 14:16:38 +0200

    Add ProjectData

---
+++ src/app/data/ProjectData.tsx
@@ -0,0 +1,23 @@
+import { ProjectProps } from "@/app/components/Project";
+
+import PortfolioImage from "@/app/assets/placeholder_Portfolio.png";
+import OldPortfolioImage from "@/app/assets/old_Portfolio.png";
+
+export const projects: ProjectProps[] = [
+    {
+        title: "Portfolio",
+        url: "https://google.com",
+        urlShorthand: "google.com",
+        github: "https://github.com/henrygraesberg/react_portfolio",
+        image: PortfolioImage,
+        technologies: ["logos:nextjs-icon", "logos:react", "logos:typescript-icon", "devicon:tailwindcss"]
+    },
+    {
+        title: "Vanilla Portfolio",
+        url: "https://henrygraesberg.github.io/Portfolio/",
+        urlShorthand: "henrygraesberg.github.io",
+        github: "https://github.com/henrygraesberg/Portfolio",
+        image: OldPortfolioImage,
+        technologies: ["logos:html-5", "logos:css-3", "logos:javascript"]
+    }
+]
