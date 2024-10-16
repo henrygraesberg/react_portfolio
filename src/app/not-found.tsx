9292b96764308541da5d85524658e6f70f2f6f1c
Henry Græsberg <henrygr@stud.ntnu.no>
2024-10-16 22:54:47 +0200

    change 404 redirect to be client side

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
-'use client'
-
-import { redirectToHome } from "./actions";
-
 const NotFound = () => {
-    redirectToHome();
+    return (
+        <div>
+        <h1>404 - Not Found</h1>
+        </div>
+    );
 };
 
 export default NotFound;
