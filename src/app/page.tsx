e3f40660ce44fb462f5e3835827c3df332e49d61
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-10-17 16:52:26 +0200

    Move hero text to above hero image ::after

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ const Home = () => {
     <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
     <main className={`bg-background dark:bg-background_dark grid grid-cols-2 pb-24 -z-30 ${darkMode && "dark"} transition-colors duration-700`}>
     <div className="col-span-2 h-[100vh] px-[5vw] flex flex-col justify-center hero">
-        <div className="font-ultra sticky">
+        <div className="font-ultra z-10">
           <h3 className="text-3xl text-secondary">Hello world, I&apos;m</h3>
           <h1 className="text-7xl text-accent">Henry<br />Græsberg</h1>
         </div>
