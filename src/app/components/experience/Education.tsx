261724247f2f211893504e5e4f97fc18de4fd058
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-10-28 12:02:21 +0100

    Change experience titles from string to JSX.Element

--- src/app/components/experience/Education.tsx
+++ src/app/components/experience/Education.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
 export interface EducationCardProps {
-    title: string
+    title: JSX.Element
     school: string
     time_span: string
 }
@@ -9,7 +9,7 @@ export interface EducationCardProps {
 const EducationCard: FC<EducationCardProps> = ({ title, school, time_span }) => {
   return (
     <div className="flex flex-col p-4 gap-1 bg-secondary rounded-xl">
-        <h3 className="text-xl text-accent font-bold">{title}</h3>
+        <h3 className="text-xl text-text font-bold">{title}</h3>
         <p className="text-text_secondary font-bold">{school}</p>
         <p className="text-text_secondary font-bold">{time_span}</p>
     </div>
--- src/app/data/EducationData.tsx
+++ src/app/data/EducationData.tsx
@@ -2,12 +2,12 @@ import { EducationCardProps } from '@/components/experience/Education';
 
 export const Education: EducationCardProps[] = [
     {
-        title: "Bachelor in Informatics",
+        title: <span>Bachelor in <span className="text-accent">Informatics</span></span>,
         school: "Norwegian University of Science and Technology",
         time_span: "Aug. 2024 - present"
     },
     {
-        title: "High School Diploma",
+        title: <span className="text-accent">High School Diploma</span>,
         school: "Blindern Videregående Skole",
         time_span: "Aug. 2021 - Jun. 2024"
     }
