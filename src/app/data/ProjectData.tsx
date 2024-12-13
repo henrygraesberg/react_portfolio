860cbf3b097f0812a0b84a03ca11335ac01e77dd
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2024-12-13 16:24:51 +0100

    replace placeholder image for shotegories

--- src/app/data/ProjectData.tsx
+++ src/app/data/ProjectData.tsx
@@ -2,6 +2,7 @@ import { ProjectProps } from "@/components/Project";
 
 import PortfolioImage from "@/assets/react_portfolio.png";
 import OldPortfolioImage from "@/assets/old_Portfolio.png";
+import ShotegoriesImage from "@/assets/shotegories_screenshot.png"
 
 export const projects: ProjectProps[] = [
     {
@@ -9,7 +10,7 @@ export const projects: ProjectProps[] = [
         url: "https://shotegories.graesberg.com",
         urlShorthand: "shotregories.graesberg.com",
         github: "https://git.graesberg.com/deno-shotegories",
-        image: PortfolioImage,
+        image: ShotegoriesImage,
         technologies: ["logos:deno", "logos:typescript-icon"],
         technologiesTooltip: ["Deno", "TypeScript"],
         deployment: ["logos:deno"],
